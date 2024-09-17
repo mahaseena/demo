4affe9ce0431ab74cc438ff6b7b8dc3dd934be98
mahaseena <mahaseena21@gmail.com>
2024-09-17 18:42:23 +0530

    add x

---
+++ x.py
@@ -0,0 +1,12 @@
+#from numpy import *
+#c=array([[1,2,3],[4,5,6],[7,8,9]])
+#print(c)
+#f=5.6790
+#print(f"{f:.2f}") #to acesse 2 decimal points
+#c=array([(1,2),(3,4)],dtype=[('a','<i4'), ('b', '<i4')], ndmin=2)
+#print(c['a'])
+#d=array([1,2,3])
+#print(c)
+#print(d)
+a=eval(input('enetr a number:'))
+print(type(a))
